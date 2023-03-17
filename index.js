613475aa5304d9c8618735751e58bf74a6342854
Naulikha <nbiketia@gmail.com>
2023-03-17 23:30:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  const returnsANamedFunction = (namedfunction) => returnsANamedFunction 
+
+  const returnsAnAnonymousFunction = () => function (){}
